59e5292e6520dda89676efecd4a829a2aacc890d
Judge40 <Judge40@users.noreply.github.com>
2017-09-11 20:26:51 +0100

    Bump mod version to 1.9.4-3.0.0
    Update build.gradle and ModInfoConstants to update the mod version from
    1.9.4.3.0.0-0.1.0 to 1.9.4-3.0.0 to indicate a formal release.

--- src/main/java/com/judge40/minecraft/bettermobgriefinggamerule/common/ModInfoConstants.java
+++ src/main/java/com/judge40/minecraft/bettermobgriefinggamerule/common/ModInfoConstants.java
@@ -36,5 +36,5 @@ public class ModInfoConstants {
 
   public static final String MINECRAFT_VERSION = "1.9.4";
 
-  public static final String VERSION = "1.9.4-3.0.0-0.1.0";
+  public static final String VERSION = "1.9.4-3.0.0";
 }

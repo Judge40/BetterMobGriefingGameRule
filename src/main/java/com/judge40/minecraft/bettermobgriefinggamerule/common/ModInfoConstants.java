7b087dfb3aa7126d1f47d81cd1b79e5e3265aeb2
Judge40 <Judge40@users.noreply.github.com>
2017-10-12 05:15:21 +0100

    Bump the mod version to 1.11.2-5.0.0
    Update build.gradle and ModInfoConstants to bump the mod version number
    from 1.11.2-5.0.0-0.1.0 to 1.11.2-5.0.0.

--- src/main/java/com/judge40/minecraft/bettermobgriefinggamerule/common/ModInfoConstants.java
+++ src/main/java/com/judge40/minecraft/bettermobgriefinggamerule/common/ModInfoConstants.java
@@ -36,5 +36,5 @@ public class ModInfoConstants {
 
   public static final String MINECRAFT_VERSION = "1.11.2";
 
-  public static final String VERSION = "1.11.2-5.0.0-0.1.0";
+  public static final String VERSION = "1.11.2-5.0.0";
 }
